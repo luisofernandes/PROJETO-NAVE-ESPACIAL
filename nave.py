6db961fea1f4112498e3d9ffe4680e33220d0262
luisfernandes <luis.o.fernandes@edu.senai.br>
2026-05-29 11:02:12 -0300

    Commit inicial - criei as funções básicas da nave

---
+++ nave.py
@@ -0,0 +1,64 @@
+## Definir as variáveis
+combustivel = 100
+tripulantes = []
+
+## Definir funções
+
+def viajar():
+    ##Aqui vamos gastar combustível
+    global combustivel ## Avisa a funçao que vamos modificar um variáveç externa
+    if(combustivel>=30):
+        combustivel = combustivel - 30
+        print("A nave viajou")
+    else:
+        print("Você está sem combustivel suficiente. Abasteça!")
+
+
+
+
+    
+def abastecer():
+    global combustivel
+    combustivel = 100
+    print("Tanque cheio ⛽")
+
+def status_nave():
+    print("\n----------- STATUS DA NAVE -----------")
+    print(f"Temos {combustivel} de combustivel")
+    print(f"Os tripulantes são: {tripulantes}")
+    print("------------------------------ \n")
+
+def registrarTripulante():
+    ##Essa função pergunta o nome do tripulante e adiciona na lista de tripulantes
+    novoTripulante = input("Qual o nome do novo tripulante?: ") #Pergunta quem
+    tripulantes.append(novoTripulante) #Inserimos o fulaninho
+    print("Tripulante inserido com sucesso! 🚀")
+
+
+##Criar um menu
+
+while True: ##esse loop roda para sempre!
+    print("\nBem vindo ao menu interativo da nave. Por favor selecione uma opção:")
+    print("\n1- Mostrar status da nave | 2- Viajar | 3- Abastecer | 4- Novo tripulante | 5- Sair")
+    opcao = input("Escolha: ")
+   
+    if (opcao == "1"):
+        status_nave()
+    elif (opcao == "2"):
+        viajar()
+    elif (opcao == "3"):
+        abastecer()
+    elif (opcao == "4"):
+        registrarTripulante()
+    elif (opcao == "5"):
+        print("Viagem encerrada!")
+        break
+
+
+
+
+# status_nave()
+# registrarTripulante()
+# registrarTripulante()
+# registrarTripulante()
+# status_nave()
